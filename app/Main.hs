bc9df0673d106c3b804aec3677ac513afac30b93
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-05-03 09:14:37 +0200

    Solve Day4

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 import           Day1           (solve)
 import           Day2           (solve)
 import           Day3           (solve)
+import           Day4           (solve)
 
 import           Utils.Solution (Solver, showSolution)
 
@@ -11,7 +12,7 @@ main = do
   let solution = (solvers !! (day - 1)) input
   putStrLn $ showSolution solution
   where
-    day = 3
+    day = 4
 
 solvers :: [Solver]
-solvers = [Day1.solve, Day2.solve, Day3.solve]
+solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve]
---
+++ src/Day4.hs
@@ -0,0 +1,58 @@
+module Day4 (solve) where
+import           Text.Parsec    (Parsec, char, digit, many1, parse)
+import           Utils.Solution (Solver)
+
+solve :: Solver
+solve input = let
+  range = parseRange input
+  part1 = uncurry solve1 range
+  part2 = uncurry solve2 range
+  in (show part1, show part2)
+
+-- Parsing ---------------
+
+parseRange :: String -> (Int, Int)
+parseRange input = case parse rangeParser "" input of
+  Left err     -> error $ show err
+  Right result -> result
+
+rangeParser :: Parsec String () (Int, Int)
+rangeParser = do
+  low <- many1 digit
+  _ <- char '-'
+  high <- many1 digit
+  return (read low, read high)
+
+-- Main -------------------------
+
+solve1 :: Int -> Int -> Int
+solve1 low high = length $ filterPasswords low high [not . hasDescending, hasDouble]
+
+solve2 :: Int -> Int -> Int
+solve2 low high = length $ filterPasswords low high [not . hasDescending, hasExactDouble]
+
+-- General ----------------------
+filterPasswords :: Int -> Int -> [String -> Bool] -> [String]
+filterPasswords low high = foldr filter passwords
+  where
+    passwords = map show [low .. high]
+
+hasDescending :: String -> Bool
+hasDescending (x : y : xs)
+  | x > y = True
+  | otherwise = hasDescending (y : xs)
+hasDescending _ = False
+
+hasDouble :: String -> Bool
+hasDouble (x : y : xs)
+  | x == y = True
+  | otherwise = hasDouble (y : xs)
+hasDouble _ = False
+
+hasExactDouble :: String -> Bool
+hasExactDouble (x : y : z : xs)
+  | x == y && x == z = hasExactDouble (dropWhile (== x) xs) -- >2 found, drop all instances
+  | x == y = True -- ==2 found, done
+  | otherwise = hasExactDouble (y : z : xs)
+hasExactDouble [x, y] = x == y -- Only two elements left, has exact double if they are equal
+hasExactDouble _ = False
