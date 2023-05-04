bcc7d6630855408ccff54d96e0b324fea088fbe2
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-05-05 00:41:04 +0200

    Finish Day3 factor, improve time by a factor 100

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,15 +1,12 @@
 module Day3 (solve) where
-import           Data.HashSet   (HashSet)
-import qualified Data.HashSet   as HS
-import           Data.List      (elemIndex, find, minimumBy)
-import           Data.Maybe     (catMaybes, mapMaybe)
-import           Data.Ord       (comparing)
-import           Debug.Trace    (trace)
-import           Text.Parsec    (Parsec, anyChar, char, digit, many1, parse,
-                                 sepBy)
-import           Utils.Geometry (Point, Vector, distanceFromOrigo, downV, leftV,
-                                 move, origo, rightV, scaleBy, upV)
-import           Utils.Solution (Solver)
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import           Text.Parsec         (Parsec, anyChar, char, digit, many1,
+                                      parse, sepBy)
+import           Utils.Geometry      (Point, distanceBetween, distanceFromOrigo,
+                                      downV, leftV, move, origo, rightV,
+                                      scaleBy, upV)
+import           Utils.Solution      (Solver)
 
 solve :: Solver
 solve input = let
@@ -25,7 +22,7 @@ data Direction = U | R | D | L
 
 data Instruction = I Direction Integer
 
-data WireSegment = WS (Point Integer) (Point Integer)
+data WireSegment = WS Integer (Point Integer) (Point Integer)
   deriving (Show)
 
 type Wire = [WireSegment]
@@ -34,11 +31,11 @@ parseWireSegments :: [Instruction] -> Wire
 parseWireSegments = reverse . foldl appendWireSegment []
 
 appendWireSegment :: Wire -> Instruction -> Wire
-appendWireSegment xs (I dir dist) = WS startPoint endPoint : xs
+appendWireSegment xs (I dir dist) = WS distFromStart startPoint endPoint : xs
   where
-    startPoint = case xs of
-      []           -> origo
-      (WS _ p) : _ -> p
+    (startPoint, distFromStart) = case xs of
+      []             -> (origo, 0)
+      (WS d s p) : _ -> (p, d + distanceBetween s p)
     endPoint = move startPoint vector
     vector = case dir of
       U -> scaleBy upV dist
@@ -78,35 +75,51 @@ directionParser = do
 -------------------------
 
 solve1 :: Wire -> Wire -> Integer
-solve1 wire1 wire2 = minimum $ HS.map distanceFromOrigo intersections --minimum $ HS.map distanceFromOrigo intersections
+solve1 wire1 wire2 = minimum $ map distanceFromOrigo $ HM.keys intersections
   where
     intersections = findIntersections wire1 wire2
 
 solve2 :: Wire -> Wire -> Integer
-solve2 wire1 wire2 = 5
+solve2 wire1 wire2 = minimum $ HM.elems intersections
+  where
+    intersections = findIntersections wire1 wire2
 
 -- General ----------------------------------
 
-data Intersection = IS (Point Integer) Integer
+type Intersection = (Point Integer, Integer)
 
-findIntersections :: Wire -> Wire -> HashSet Intersection
-findIntersections wire1 wire2 = HS.delete (0, 0) $ HS.fromList $ foldl (findIntersections' wire2) ([], 0) wire1
+findIntersections :: Wire -> Wire -> HashMap (Point Integer) Integer
+findIntersections wire1 wire2 =
+  HM.delete (0, 0) $ HM.fromList $ concatMap (findIntersections' wire2) wire1
 
-findIntersections' :: Wire -> ([Point Integer], Integer) -> WireSegment -> [Point Integer]
-findIntersections' xs x = concatMap (findIntersection x) xs
+findIntersections' :: Wire -> WireSegment -> [Intersection]
+findIntersections' wire wireSeg = concatMap (findIntersection wireSeg) wire
 
-findIntersection :: WireSegment -> WireSegment -> [Point Integer]
+findIntersection :: WireSegment -> WireSegment -> [Intersection]
 findIntersection w1 w2
   | intersects w1 w2 = findIntersection' w1 w2
   | otherwise = []
 
-findIntersection' :: WireSegment -> WireSegment -> [Point Integer]
-findIntersection' w1@(WS (startX1, startY1) (endX1, endY1)) w2@(WS (startX2, startY2) (endX2, endY2))
-  | isHorizontal w1 && isHorizontal w2 = [(x, startY1) | x <- [max minX1 minX2 .. min maxX1 maxX2]]
-  | isVertical w1 && isVertical w2 = [(startX1, y) | y <- [max minY1 minY2 .. min maxY1 maxY2]]
-  | isHorizontal w1 && isVertical w2 = [(startX2, startY1)]
-  | otherwise = [(startX1, startY2)]
+-- The worst, most unreadable code I've ever written
+findIntersection' :: WireSegment -> WireSegment -> [Intersection]
+findIntersection'
+  w1@(WS dist1 (startX1, startY1) (endX1, endY1))
+  w2@(WS dist2 (startX2, startY2) (endX2, endY2))
+  | isHorizontal w1 && isHorizontal w2 =
+    [((x, startY1), baseDist + abs (x - startX1) + abs (x - startX2)) |
+      x <- [max minX1 minX2 .. min maxX1 maxX2]]
+  | isVertical w1 && isVertical w2 =
+    [((startX1, y), baseDist + abs (y - startY1) + abs (y - startY2)) |
+      y <- [max minY1 minY2 .. min maxY1 maxY2]]
+  | isHorizontal w1 && isVertical w2 = let
+    dist' = baseDist + abs (startX1 - startX2) + abs (startY1 - startY2)
+    in [((startX2, startY1), dist')]
+  | otherwise = let
+    dist' = baseDist + abs (startX1 - startX2) + abs (startY1 - startY2)
+    in [((startX1, startY2), dist')]
   where
+    baseDist = dist1 + dist2
+
     maxX1 = max startX1 endX1
     minX1 = min startX1 endX1
     maxX2 = max startX2 endX2
@@ -118,13 +131,13 @@ findIntersection' w1@(WS (startX1, startY1) (endX1, endY1)) w2@(WS (startX2, sta
     minY2 = min startY2 endY2
 
 isHorizontal :: WireSegment -> Bool
-isHorizontal (WS (_, y1) (_, y2)) = y1 == y2
+isHorizontal (WS _ (_, y1) (_, y2)) = y1 == y2
 
 isVertical :: WireSegment -> Bool
 isVertical = not . isHorizontal
 
 intersects :: WireSegment -> WireSegment -> Bool
-intersects (WS (startX1, startY1) (endX1, endY1)) (WS (startX2, startY2) (endX2, endY2)) =
+intersects (WS _ (startX1, startY1) (endX1, endY1)) (WS _ (startX2, startY2) (endX2, endY2)) =
   overlapsX && overlapsY
   where
     maxX1 = max startX1 endX1
